283b5d1a49b2fc91dd34c0e520c30d4e76fe29dd
Ali Nouri <alinooritataloo@gmail.com>
2025-08-01 01:17:50 +0000

    When I go to the `dashboard/login` page and login, it doesn't redirect m

--- src/app/dashboard/_actions/auth.ts
+++ src/app/dashboard/_actions/auth.ts
@@ -1,7 +1,9 @@
+
 "use server";
 
 import { signIn, signOut } from "@/../auth";
 import { AuthError } from "next-auth";
+import { redirect } from "next/navigation";
 
 export async function authenticate(
   prevState: string | undefined,
@@ -14,12 +16,20 @@ export async function authenticate(
       switch (error.type) {
         case "CredentialsSignin":
           return "نام کاربری یا گذرواژه نامعتبر است.";
+        case "CallbackRouteError":
+            // This can happen on successful login. NextAuth.js throws this error
+            // and then redirects. We can safely ignore it, but we need to redirect
+            // manually in the happy path.
+            break;
         default:
           return "خطایی رخ داده است. لطفا دوباره تلاش کنید.";
       }
     }
+    // Re-throw other errors
     throw error;
   }
+  // This will only be reached on successful login.
+  redirect("/dashboard");
 }
 
 export async function logout() {
